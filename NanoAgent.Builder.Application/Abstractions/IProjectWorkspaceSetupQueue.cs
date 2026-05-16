b90a7960fac2938e6d04856633a3a9880c819d73
Muhammad Rizwan <allgame45@hotmail.com>
2026-05-16 20:14:28 +0500

    Expose setup dequeue

--- NanoAgent.Builder.Application/Abstractions/IProjectWorkspaceSetupQueue.cs
+++ NanoAgent.Builder.Application/Abstractions/IProjectWorkspaceSetupQueue.cs
@@ -3,4 +3,6 @@ namespace NanoAgent.Builder.Application.Abstractions;
 public interface IProjectWorkspaceSetupQueue
 {
     ValueTask QueueAsync(Guid projectId, CancellationToken cancellationToken = default);
+
+    Task<Guid> DequeueAsync(CancellationToken cancellationToken = default);
 }
